34c8ab135fd437c00df098c90af360e96cb43e7e
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-05 18:34:57 +0700

    featL add text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="w-full h-[100vh] flex items-center justify-center">
-      <h1 className="font-semibold text-3xl">We Are Cooking Something Good!</h1>
+    <main className="w-full h-[100vh] flex items-center justify-center flex-col">
+      <small>Haha, this domain is taken</small>
+      <h1 className="font-semibold text-2xl md:text-3xl text-center">We Are Cooking Something Good! 👨‍🍳🍳</h1>
     </main>
   );
 }
